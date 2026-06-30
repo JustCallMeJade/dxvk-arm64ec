d2024dd099d9f5c23547912bfc4ac5b0f9fc3e44
JustCallMeJade <rotheory13@gmail.com>
2026-06-30 19:47:32 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+workdir="$(pwd)/dxvk-workdir"
+install_dir="$workdir/install"
+
+mkdir -p "$workdir"
+
+cd workdir
+
+apt update && apt upgrade -y -qq
+
+apt install build-essential cmake wget unzip tar meson ninja-build glslang-tools git -y -qq
+
+wget https://github.com/mstorsjo/llvm-mingw/releases/download/20260616/llvm-mingw-20260616-ucrt-ubuntu-22.04-aarch64.tar.xz -qnv
+
+tar -xf llvm-mingw-20260616-ucrt-ubuntu-22.04-aarch64.tar.xz 
+
+export PATH="$PATH:$workdir/llvm-mingw-20260616-ucrt-ubuntu-22.04-aarch64/bin"
