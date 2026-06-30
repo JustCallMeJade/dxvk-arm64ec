fa58885f2952dbb40fabebdb5c3c6949198fc38a
JustCallMeJade <rotheory13@gmail.com>
2026-06-30 20:07:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 workdir="$(pwd)/dxvk-workdir"
 install_dir="$workdir/install"
 
+rm rf "$workdir"
 mkdir -p "$workdir"
 cd "$workdir"
 
@@ -10,11 +11,15 @@ apt update && apt upgrade -y -qq
 
 apt install build-essential cmake wget unzip tar meson ninja-build glslang-tools git -y -qq
 
+echo "installing mingw cross compilers"
+
 wget https://github.com/mstorsjo/llvm-mingw/releases/download/20260616/llvm-mingw-20260616-ucrt-ubuntu-22.04-aarch64.tar.xz -qnv
 
 tar -xf llvm-mingw-20260616-ucrt-ubuntu-22.04-aarch64.tar.xz
 
 export PATH="$PATH:$workdir/llvm-mingw-20260616-ucrt-ubuntu-22.04-aarch64/bin"
+
+echo "cloning dxvk"
 
 git clone --recursive --depth=1 https://github.com/doitsujin/DXVK
 
@@ -40,6 +45,8 @@ cpu = 'armv8'
 endian = 'little'
 EOF
 
+echo "configuring and compiling"
+
 meson setup dxvk-arm64ec -Dbuildtype=release -Dstrip=enabled -Denable_d3d8=false --prefix="$install_dir" --cross-file build-win64.txt
 
 ninja -C dxvk-arm64ec install
@@ -49,3 +56,7 @@ cd "$install_dir"
 mv bin system32
 
 zip -r "$workdir/dxvk-arm64ec.zip" system32
+
+cd
+
+exit 0
